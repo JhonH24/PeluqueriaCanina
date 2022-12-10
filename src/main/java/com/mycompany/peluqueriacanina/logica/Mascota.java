f74b94c303b66021411b3241f849b1ff2246ba0a
JhonH24 <jhonh0807@gmail.com>
2022-12-10 20:37:41 -0300

    Clases, estructura de 3 capas y mapeo ORM

--- src/main/java/com/mycompany/peluqueriacanina/logica/Mascota.java
+++ src/main/java/com/mycompany/peluqueriacanina/logica/Mascota.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 
 @Entity
 public class Mascota {
@@ -17,6 +18,7 @@ public class Mascota {
     private String atencion_especial;
     private String observaciones;
     
+    @OneToOne
     private Duenio unDuenio; // estableciendo la relacion 1 a 1
 
     public Mascota() {
